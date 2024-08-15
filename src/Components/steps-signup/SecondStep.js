0e453e7aaedff37ac774be957a471870341b816e
MeEtSoNi-lab <meet@tulong.tech>
2024-08-15 10:43:57 -0400

    Added eye icon  chan

--- src/Components/steps-signup/SecondStep.js
+++ src/Components/steps-signup/SecondStep.js
@@ -1,44 +1,63 @@
-import React ,{useContext}from 'react'
-import './steps.css'
-import { multiStepContext } from '../../StepContext'
+import React, { useContext, useState } from 'react';
+import './steps.css';
+import { multiStepContext } from '../../StepContext';
 
 function SecondStep() {
-    const{setcurrentStep,userData,setuserData, handleChnage} = useContext(multiStepContext);
+    const { setcurrentStep, userData, setuserData, handleChnage } = useContext(multiStepContext);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  return (
-    <div className='signup-container'>
-    <div className="signup-contentBox">
-        <div className="signup-h1">
-       
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword(!showConfirmPassword);
+    };
+
+    return (
+        <div className='signup-container'>
+            <div className="signup-contentBox">
+                <div className="signup-h1">
+                    {/* You can add a title here */}
+                </div>
+                <div className="signup-form">
+                    <div className="password-container">
+                        <input
+                            value={userData.password}
+                            onChange={handleChnage}
+                            name='password'
+                            placeholder='Password'
+                            type={showPassword ? "text" : "password"}
+                        />
+                        <span className="eye-icon" onClick={togglePasswordVisibility}>
+                            {showPassword ? <i class="fa-solid fa-eye"></i> : <i class="fa-solid fa-eye-slash"></i>}
+                        </span>
+                    </div>
+                    <div className="password-container">
+                        <input
+                            value={userData.ConfirmPassword}
+                            onChange={handleChnage}
+                            name='ConfirmPassword'
+                            placeholder='Confirm Password'
+                            type={showConfirmPassword ? "text" : "password"}
+                        />
+                        <span className="eye-icon" onClick={toggleConfirmPasswordVisibility}>
+                            {showConfirmPassword ? <i class="fa-solid fa-eye"></i>: <i class="fa-solid fa-eye-slash"></i>}
+                        </span>
+                    </div>
+                </div>
+                <div className='next-back-buttons'>
+                    <button className="submit-button" onClick={() => setcurrentStep(1)}>
+                        Back
+                    </button>
+                    <button className="submit-button" onClick={() => { setcurrentStep(3) }}>
+                        Next
+                    </button>
+                </div>
+            </div>
         </div>
-      
-            <div className="signup-form">
-            
-                
-                <input  value={userData.password} onChange={handleChnage} name='password' placeholder='Password' type="password" />
-                <input value={userData.ConfirmPassword} onChange={handleChnage} name='ConfirmPassword'  placeholder='Confirm Password' type="password" />
-
-            </div>
-            <div className='next-back-buttons'>
-            <button className="submit-button" onClick={()=>setcurrentStep(1)} >
-                    Back
-                </button>
-                <button className="submit-button" onClick={()=>{setcurrentStep(3)}}>
-                    Next
-                </button>
-            </div>
-        
-    </div>
-    {/* {showPopup && (
-    <div className="popup">
-      <div className="popup-content">
-        <h2>Signup Successful</h2>
-        <button className='close-button' onClick={closePopup}>Close</button>
-      </div>
-    </div>
-  )} */}
-    </div>
-  )
+    );
 }
 
-export default SecondStep
+export default SecondStep;
